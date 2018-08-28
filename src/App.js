af28aacab17607db14d6b925bc1d7d0f8235e0de
Alchemist21 <35514769+Alchemist21@users.noreply.github.com>
2018-08-28 01:34:17 -0400

    version1

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './SearchBox';
+import './App.css';
+import Scroll from './Scroll';
+
+
+class App extends Component {
+  constructor () {
+  	super()
+  	this.state = {
+  		robots: robots,
+  		searchfield: ''
+  	}
+  }
+
+  onSearchChange = (event) => {
+  		this.setState({ searchfield: event.target.value})
+  }
+ 
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot =>{
+  		return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+  	})
+
+    return (
+	  <div className='tc'>
+		<h1 className='f1'> Friends</h1>
+		<SearchBox  searchChange={this.onSearchChange}/>
+		
+		<Scroll>
+			<CardList robots={filteredRobots}/>
+		</Scroll>
+      </div>
+	
+	);
+  }
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [
+{
+ id: 1,
+ name: 'Louell Sala',
+ username:'Alchemist',
+ email:'louell@mailcity.com'
+},
+{
+ id: 2,
+ name: 'Alvin Rey Tan',
+ username:'Bin',
+ email:'artmd@gmail.com'
+
+},
+{
+ id: 3,
+ name:'Dan Koifman',
+ username:'Danny',
+ email:'danny@gmail.com'
+
+},
+{
+ id: 4,
+ name:'Louie Tan',
+ username:'PPG',
+ email:'paulg@gmail.com'
+
+},
+{
+ id: 5,
+ name:'Roselle Andres',
+ username:'Rosy',
+ email:'Roselle@gmail.com'
+
+}
+
+
+
+]
